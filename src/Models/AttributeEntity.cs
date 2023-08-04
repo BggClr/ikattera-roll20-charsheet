b6aa338649a68dfa8bc03eca222ed23f0ff35863
Danil Zhelnov <zhelnovda@gmail.com>
2023-08-04 17:48:11 +0400

    Working with skills. reorganised source json

--- src/Models/AttributeEntity.cs
+++ src/Models/AttributeEntity.cs
@@ -4,5 +4,5 @@ public class AttributeEntity
 {
 	public string Title { get; set; }
 	public string Name { get; set; }
-	public IDictionary<int, Bonus> LevelBonus { get; set; }
+	public IDictionary<int, IDictionary<string, int>> LevelBonus { get; set; }
 }
