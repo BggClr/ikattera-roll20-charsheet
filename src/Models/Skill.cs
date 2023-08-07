713033bedd9a2cf258e82b6ab7d1706af095569b
Danil Zhelnov <zhelnovda@gmail.com>
2023-08-07 14:41:09 +0400

    Changed visuals - removed 3rd column. Added tabs to skills

--- src/Models/Skill.cs
+++ src/Models/Skill.cs
@@ -6,5 +6,7 @@ public class Skill : CharacterFeature
 {
 	[JsonPropertyName("depends_on")]
 	public IList<string> DependsOn { get; set; }
+	[JsonPropertyName("custom_type")]
+	public IList<string> CustomType { get; set; }
 	public override string FeatureType => "skill";
 }
